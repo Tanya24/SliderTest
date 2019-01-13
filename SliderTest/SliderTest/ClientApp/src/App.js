970ecba25fafa9abee0df4f199b09561d80420dd
Tanya24 <tanuffko24@gmail.com>
2019-01-13 17:36:10 +0500

    Sample from a lesson

--- SliderTest/SliderTest/ClientApp/src/App.js
+++ SliderTest/SliderTest/ClientApp/src/App.js
@@ -4,17 +4,32 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+//import styled from 'styled-components';
+
 
 export default class App extends Component {
   displayName = App.name
 
   render() {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetchdata' component={FetchData} />
-      </Layout>
+      return (
+          <div className="wrapper">
+              <SayFullName name="Tanya" surname="Kochergina" link="test@test.ru" />
+              <SayFullName name="Tanya2" surname="K2" link="test2@test.ru" />
+              <SayFullName name="Tanya3" surname="K3" link="test3@test.ru" />
+          </div>
     );
   }
 }
+
+function SayFullName(props) {
+    return (
+        <div>
+            <h1> My name - {props.name}, My surname - {props.surname}</h1>
+            <a href={props.link}>My link</a>
+        </div>
+        )
+}
+    
